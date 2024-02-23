02e614aee9e6b71eb527035ccdf372abd8c54544
Arjun-4366 <132777262+Arjun-4366@users.noreply.github.com>
2024-02-23 16:28:54 +0530

    Added login form using react hook-useState

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import "./App.css";
 
 function App() {
   let [pstatus, setPStatus] = useState(false);
+  let [login,setLogin] = useState(false)
+
   return (
     <div className="App ">
+      <button className="reg-btn" onClick={()=>setLogin(!login)}>Register here</button>
+      <div className={`overlay ${login ?"showOverLay":''}`}></div>
+      <div className={`loginModal ${login ?"showLogin":''}`}>
+        <div className="title-div">
+        <h1>Register Here</h1>
+        <button className="cross-btn" onClick={()=>setLogin(!login)}>&times;</button>
+        </div>
+        <div className="login-form">
+          <form action="">
+          <input type="text" placeholder="Enter email here"/>
+          <input type="password" placeholder="Enter your password"/>
+          <div className="submit-btn-div">
+          <button className="submit-btn">submit</button>
+          </div>
+          
+          </form>
+        
+        </div>
+      
+    
+      </div>
       <button className="micon" onClick={()=>setPStatus(!pstatus)}>
         {
          pstatus?<span>&times;</span>:<span>&#9776;</span>
